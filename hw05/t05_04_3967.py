faf37e527c82aa8e46586cd60e1714e2558088b6
Yuliia Kachurovska <144264049+yuliakachurovska@users.noreply.github.com>
2025-02-07 10:56:01 +0200

    Added hw05

---
+++ hw05/t05_04_3967.py
@@ -0,0 +1,35 @@
+def find_max_length(n, k, array):
+    """Знаходить максимальну довжину шматків мотузки, яку можна нарізати. 
+    
+    :param n: Кількість білизняних мотузок (1 ≤ n ≤ 10001).
+    :param k: Кількість будиночків  (1 ≤ k ≤ 10001).
+    :param array: Не відсортований масив довжин наявних мотузок у сантиметрах (1 ≤ array[i] ≤ 10^7).
+    :return: Максимальна довжина мотузки, яку можна нарізати, 
+        або 0, якщо довжини не вистачить на k відрізків довжини 1. 
+    """
+    left = 1
+    right = max(array)
+
+    while left <= right:
+        m = left + (right - left) // 2
+        sum = 0
+        for i in array:
+            sum += i // m   # 500 // 200 -> 2, 450 // 200 -> 2, 250 // 200 -> 1 ==> 5
+        
+        if sum >= k:  # 5 7
+            left = m + 1
+        else:
+            right = m - 1
+
+    return right
+
+if __name__ == '__main__':
+    n, k = [int(i) for i in input().split()]
+    arr = [int(input()) for _ in range(n)]
+    print(find_max_length(n, k, arr))
+
+    # 4 11
+    # 802 4 2
+    # 743 3 2
+    # 457 2 1
+    # 539 2 1
